c6f3b4fe9315e8146d2422844c28620c62f8cb73
Jonathan Perea <john.perea00@gmail.com>
2024-12-10 21:52:19 -0600

    added sky to backgorund and saved 3d models for future use

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Routes} from 'react-router-dom'
 import { Navbar } from './components/Navbar'
+import { Home, About, Project, Contact } from './pages'
+
 
 const App = () => {
   return (
@@ -8,10 +10,10 @@ const App = () => {
         <Router>
             <Navbar/>
             <Routes>
-                <Route path="/" element = {'Home'}/>
-                <Route path="/about" element = {'About'}/>
-                <Route path="/projects" element = {'Projects'}/>
-                <Route path="/contact" element = {'Contacts'}/>
+                <Route path="/" element = {<Home/>}/>
+                <Route path="/about" element = {<About/>}/>
+                <Route path="/projects" element = {<Project/>}/>
+                <Route path="/contact" element = {<Contact/>}/>
             </Routes>
         </Router>
     </main>
