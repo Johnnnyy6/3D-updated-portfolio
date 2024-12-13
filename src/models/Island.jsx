39b041d7ffdd4e04c30fcf3c0b2a625309811dc3
Jonathan Perea <john.perea00@gmail.com>
2024-12-12 21:59:51 -0600

    added plane animation and sky rotation whenever we are moving plane for visual affect

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -13,7 +13,7 @@ import islandScene from '../assets/3d/island.glb'
 import {a} from '@react-spring/three'
 
 
-const Island = ({isRotating, setIsRotating, ...props}) => {
+const Island = ({isRotating, setIsRotating, setCurrentStage, ...props}) => {
   const {gl,viewport} = useThree();
   const lastX = useRef(0);
   const rotationSpeed = useRef(0);
@@ -21,7 +21,7 @@ const Island = ({isRotating, setIsRotating, ...props}) => {
   const { nodes, materials } = useGLTF(islandScene)
   const dampingFactor = 0.95;
   const handlePointerDown = (e) => {
-    e.stopProgation();
+    e.stopPropagation();
     e.preventDefault();
     setIsRotating(true);
 
@@ -29,24 +29,21 @@ const Island = ({isRotating, setIsRotating, ...props}) => {
     lastX.current = clientX;
   }
   const handlePointerUp = (e) => {
-    e.stopProgation();
+    e.stopPropagation();
     e.preventDefault();
     setIsRotating(false);
 
-    const clientX = event.touches ? event.touches[0].clientX : event.clientX;
-    const delta = (clientX - lastX.current) / viewport.width;
-    islandRef.current.rotation.y += delta * 0.01 * Math.PI;
-    lastX.current = clientX;
-    rotationSpeed.current = delta * 0.01 * Math.PI;
-
-
   }
   const handlePointerMove = (e) => {
-    e.stopProgation();
+    e.stopPropagation();
     e.preventDefault();
 
     if(isRotating){
-      handlePointerUp(e);
+      const clientX = event.touches ? event.touches[0].clientX : event.clientX;
+      const delta = (clientX - lastX.current) / viewport.width;
+      islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+      lastX.current = clientX;
+      rotationSpeed.current = delta * 0.01 * Math.PI;
     }
   }
 
@@ -60,15 +57,61 @@ const Island = ({isRotating, setIsRotating, ...props}) => {
     }
   }
 
+  const handleKeyUp = (e) => {
+    if(e.key === 'ArrowLeft' || e.key === 'ArrowRight'){
+      setIsRotating(false);
+    }
+  }
+
+  useFrame(() => {
+    if(!isRotating){
+      rotationSpeed.current *= dampingFactor;
+
+      if(Math.abs(rotationSpeed.current) < 0.001){
+        rotationSpeed.current = 0;
+      }
+
+      islandRef.current.rotation.y += rotationSpeed.current;
+    } else{
+      const rotation = islandRef.current.rotation.y;
+      
+      const normalizedRotation =
+        ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      // Set the current stage based on the island's orientation
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentStage(1);
+          break;
+        default:
+          setCurrentStage(null);
+      }
+    }
+  })
+
   useEffect(()=> {
-    document.addEventListener('pointerdown', handlePointerDown);
-    document.addEventListener('pointerup', handlePointerUp);
-    document.addEventListener('pointermove', handlePointerMove);
+    const canvas = gl.domElement;
+    canvas.addEventListener('pointerdown', handlePointerDown);
+    canvas.addEventListener('pointerup', handlePointerUp);
+    canvas.addEventListener('pointermove', handlePointerMove);
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
 
     return () => {
-      document.removeEventListener('pointerdown', handlePointerDown);
-      document.removeEventListener('pointerup', handlePointerUp);
-      document.removeEventListener('pointermove', handlePointerMove);
+      canvas.removeEventListener('pointerdown', handlePointerDown);
+      canvas.removeEventListener('pointerup', handlePointerUp);
+      canvas.removeEventListener('pointermove', handlePointerMove);
+      document.removeEventListener('keydown',handleKeyDown);
+      document.removeEventListener('keyup', handleKeyUp);
     }
 
   },[gl, handlePointerDown,handlePointerMove,handlePointerUp])
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,7 @@ import Plane from '../models/Plane'
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0,-6.5,-43];
@@ -52,13 +53,14 @@ const Home = () => {
         <ambientLight intensity={0.5}/>
         <hemisphereLight skyColor='#b1e1ff' groundColor="#000000" intensity={0.5}/>
         <Bird/>
-        <Sky/>
+        <Sky isRotating={isRotating}/>
         <Island
         position = {islandPosition}
         scale = {islandScale}
         rotation = {islandRotation}
         isRotating={isRotating}
         setIsRotating={setIsRotating}
+        setCurrentStage={setCurrentStage}
         />
         <Plane
         isRotating={isRotating}
